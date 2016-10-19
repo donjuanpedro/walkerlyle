5da2e48b535ff2b79ba44443cd439643f25c2a45
Walker <jwalkerlyle@gmail.com>
2016-10-18 21:41:38 -0500

    fixed side scroll

--- client/js/views/NavBarView.js
+++ client/js/views/NavBarView.js
@@ -7,17 +7,11 @@ const NavBarView = Backbone.View.extend({
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header" id="title">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
       <a class="navbar-brand navBarLinks" href="#">Walker Lyle</a>
     </div>
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <div>
       <ul class="nav navbar-nav">
-        <li><a class="navBarLinks" href="#">About </a></li>
+        <li><a href="#">About </a></li>
         <li>
           <a href="http://donjuanpedro.github.io/" target="_blank">Projects </a>
         </li>
@@ -29,7 +23,6 @@ const NavBarView = Backbone.View.extend({
           </form>
         </li>
       </ul>
-    </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
   `,
